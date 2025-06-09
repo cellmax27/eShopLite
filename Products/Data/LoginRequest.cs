987b4a3dd131f5e988ab71ac03ff3a79530c0442
Arnaud B <cellmax@proton.me>
2025-06-09 16:29:47 +0200

    fix

--- Products/Data/LoginRequest.cs
+++ Products/Data/LoginRequest.cs
@@ -1,5 +1,8 @@
-public class LoginRequest
+namespace Products.Data
 {
-    public string? Name { get; set; }
-    public string? Password { get; set; }
+    public class LoginRequest
+    {
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+    }
 }
